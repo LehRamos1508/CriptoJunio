3d043fcc86f1ed40c815b4f993364fa1564edd4a
Joel Neto <joelalvesneto187@gmail.com>
2025-05-17 14:25:35 -0300

    :recycle: Refactor: Update login function to include JWT token generation and improve error response structure

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import jwt from "jsonwebtoken";
+import {prisma} from "../database/index.js";
 
 export async function login(req, res, next) {
   const { username, password } = req.body;
@@ -8,10 +8,14 @@ export async function login(req, res, next) {
     const user = await prisma.user.findUnique({ where: { username } });
 
     if (!user || user.password !== password) {
-      return res.status(401).json({ error: "Credenciais inválidas" });
+      return res.status(401).json({ success: false, error: "Credenciais inválidas" });
     }
 
-    return res.status(200).json({ message: "Login bem-sucedido", userId: user.id });
+    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({ success: true, message: "Login bem-sucedido", token });
   } catch (err) {
     next(err);
   }
